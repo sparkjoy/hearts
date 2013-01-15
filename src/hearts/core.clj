02548da3edcb26af74a52a9b00d0bc2bf6c32f1b
sparkjoy <sparkjoy@pluralsight.com>
2013-01-14 18:11:18 -0800

    trick-winner, card-score

--- src/hearts/core.clj
+++ src/hearts/core.clj
@@ -5,3 +5,24 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Welcome to Hearts in Clojure! I don't do much yet..."))
+
+(def queen-rank 11)
+(def queen-score 13)
+(def queen-of-spades {:suit :s, :rank queen-rank})
+
+(defn led-suit [trick]
+  (:suit (first trick)))
+
+(defn trick-winner [trick]
+  (let [required-suit (led-suit trick)
+	indexed-trick (map #(assoc %1 :pos %2) trick (iterate inc 0))
+	suited-cards (filter #(= required-suit (:suit %)) indexed-trick)
+	ordered-by-rank (sort-by :rank > suited-cards)]
+    (:pos (first ordered-by-rank))))
+
+(defn card-score [card]
+  (case (:suit card)
+	 :h 1
+	 :s (if (= queen-rank (:rank card)) queen-score 0)
+	 0))
+
--- test/hearts/core_test.clj
+++ test/hearts/core_test.clj
@@ -2,6 +2,28 @@
   (:use clojure.test
         hearts.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest led-suit-answers-first-suit-in-trick
+  (is (= :c (led-suit [{:suit :c} {:suit :d}]))))
+
+(deftest highest-rank-in-led-suit-wins-trick
+  (is (= 1 (trick-winner [
+    {:suit :d :rank 4}
+    {:suit :d :rank 5}
+    {:suit :c :rank 6}
+    {:suit :s :rank 7}]))))
+
+(deftest led-suit-wins-trick-if-nobody-else-plays-that-suit
+  (is (= 0 (trick-winner [
+    {:suit :h :rank 4}
+    {:suit :d :rank 5}
+    {:suit :c :rank 6}
+    {:suit :s :rank 7}]))))
+
+(deftest card-score-is-one-for-a-heart
+  (is (= 1 (card-score {:suit :h}))))
+
+(deftest card-score-is-zero-for-a-club
+  (is (= 0 (card-score {:suit :c}))))
+
+(deftest card-score-for-queen-of-spades-is-thirteen
+  (is (= 13 (card-score queen-of-spades))))
